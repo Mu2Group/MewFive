189de34b986b975b7fd26fe68469469f7bc602b5
Thomas Lukasiewicz <53915559+tlukasiewicz89@users.noreply.github.com>
2022-08-30 17:12:42 -0400

    updated frontend file names

---
+++ client/components/Login.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+
+  const navigate = useNavigate();
+
+  //send login info to backend
+  const handleLogin = (event) => {
+    const loginObj = {
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value
+    };
+    axios.post(`/login`, {
+      loginObj
+    })
+    .then((userID) => {
+      console.log(userID);
+      navigate('/productFeed')
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <div className='login'>
+        <div>Log In</div>
+        <input
+          id='username'
+          type='text'
+          placeholder='Username'
+        />
+        <input
+          id='password'
+          type='password'
+          placeholder='Password'
+        />
+        <button onClick={handleLogin}  className='submit_btn'>
+            Submit
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Login;

8b5f6b23e0adc3a8c85123dcaa312082be850c63
Thomas Lukasiewicz <53915559+tlukasiewicz89@users.noreply.github.com>
2022-08-31 11:20:46 -0400

    updated add to cart to update when user already has product

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -17,7 +17,7 @@ cartController.getProducts = async (req, res, next) => {
         FROM inventory i JOIN carts c 
         ON i.productid = c.productid WHERE c.userid = ${userID};`;
         const cart = await Carts.query(sqlQuery);
-        console.log(cart.rows)
+        console.log('cart.rows from cartcontroller getproducts', cart.rows)
         res.locals.cart = cart.rows;
         return next();
     }
@@ -83,23 +83,42 @@ cartController.updateCart = async (req, res, next) => {
     //     res.status(500).send("Something went wrong");
     // }
 }
-
+// add product to user cart
 cartController.addProduct = async (req, res, next) => {
     const userID = req.params.userID;
     const productID = req.params.productID
     const { quantity } = req.body;
 
     try{
+      // check if product exists in user cart then update otherwise add new row to carts db
+      const checkCartQuery = `
+      SELECT * FROM carts 
+      WHERE userid = ${userID} 
+      AND productid = ${productID};
+      `;
+      const currCart = await Carts.query(checkCartQuery); 
+      if (currCart.rows.length) {
+        const updateQuery = `
+              UPDATE carts
+              SET quantity = quantity + ${quantity} 
+              WHERE productID = ${productID}
+              AND userid = ${userID}
+              RETURNING *
+            `; 
+        const addedProduct = await Carts.query(updateQuery); 
+        res.locals.addedProduct = addedProduct.rows[0]; 
+        return next();
+      } else {
         const params = [quantity, userID, productID];
-        console.log(params); 
         const sqlQuery = `
           INSERT INTO carts (quantity, userid, productid)
           VALUES ($1, $2, $3) RETURNING *; 
         `; 
         const addedProduct = await Carts.query(sqlQuery, params); 
-        console.log({addedProduct});
         res.locals.addedProduct = addedProduct.rows[0]; 
         return next(); 
+      }
+
 
         // let cart = await Carts.findOne({userId});
         // let product = await Inventory.findOne({_id: productId});
@@ -139,7 +158,7 @@ cartController.addProduct = async (req, res, next) => {
     }
     catch(err) {
         console.log('caught something in cartController addProduct');
-        return next('could add product to cart');
+        return next('could not add product to cart');
       }
 }
 // WORKS
